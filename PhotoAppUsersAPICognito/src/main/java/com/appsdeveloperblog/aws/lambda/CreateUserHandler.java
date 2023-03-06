4e27c2800eebbffb639ba1cb40295cf9bbb38462
mosserryan <mosser.ryan.a@gmail.com>
2023-03-06 15:55:05 -0800

    initial commit

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/CreateUserHandler.java
@@ -0,0 +1,38 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        String requestBody = input.getBody();
+        LambdaLogger logger = context.getLogger();
+        logger.log("Original json body: " + requestBody);
+
+        JsonObject userDetails = JsonParser.parseString(requestBody).getAsJsonObject();
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        return response
+                .withStatusCode(200)
+                .withBody("{}");
+
+    }
+
+}
