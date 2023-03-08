e0228651ddcf3a2d483612a50a064e058d048abe
mosserryan <mosser.ryan.a@gmail.com>
2023-03-07 17:40:32 -0800

    Added handler to allow confirmation of new user and updated service class

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/ConfirmUserHandler.java
@@ -0,0 +1,62 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.appsdeveloperblog.aws.lambda.service.CognitoUserService;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfirmUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private final CognitoUserService cognitoUserService;
+    private final String appClientId;
+    private final String appClientSecret;
+
+    public ConfirmUserHandler() {
+        this.cognitoUserService = new CognitoUserService(System.getenv("AWS_REGION"));
+        this.appClientId = System.getenv("MY_COGNITO_POOL_APP_CLIENT_ID");
+        this.appClientSecret = System.getenv("MY_COGNITO_POOL_APP_CLIENT_SECRET");
+    }
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+
+        Map<String, String> headers = new HashMap<>();
+        LambdaLogger logger = context.getLogger();
+        headers.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        String requestBody = input.getBody();
+
+        JsonObject userDetails = null;
+        try {
+            userDetails = JsonParser.parseString(requestBody).getAsJsonObject();
+            String email = userDetails.get("email").getAsString();
+            String confirmationCode = userDetails.get("code").getAsString();
+            JsonObject createUserResult = cognitoUserService.confirmUserSignup(appClientId, appClientSecret, email, confirmationCode);
+            response.withStatusCode(200);
+            response.withBody(new Gson().toJson(createUserResult, JsonObject.class));
+        } catch (AwsServiceException exception) {
+            logger.log(exception.awsErrorDetails().errorMessage());
+            response.withStatusCode(exception.awsErrorDetails().sdkHttpResponse().statusCode());
+            response.withBody(exception.awsErrorDetails().errorMessage());
+        } catch (Exception exception) {
+            logger.log(exception.getMessage());
+            response.withBody(exception.getMessage());
+            response.withStatusCode(500);
+        }
+
+        return response;
+    }
+
+
+}
